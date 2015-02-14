4f07c872acb6d4488572899cbcda4dfeaf3b3044
Joe Sak <joe@joesak.com>
2015-02-13 19:56:43 -0500

    Show workouts in json

--- app/controllers/api/v1/workouts_controller.rb
+++ app/controllers/api/v1/workouts_controller.rb
@@ -1,2 +1,5 @@
 class Api::V1::WorkoutsController < ApplicationController
+  def index
+    render json: { workouts: Workout.all }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :workouts
+      resources :workouts, defaults: { format: :json }
     end
   end
 end
